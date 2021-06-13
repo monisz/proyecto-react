1788d7e2bc7862d2f9e007d16530847200552d09
Mónica Szewczuk <mvszewczuk@gmail.com>
2021-06-13 15:17:23 -0300

    cambios clase 3 nav

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import { Nav } from './components/NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <header>
+          <Nav />
+          <br />
+          <br />
+          <h1 style= {{display: 'flex', justifyContent: 'center'}} >
+            DESAFIO CLASE 3
+          </h1>
+        </header>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,30 @@
+import './styles.css';
+const menu = ['Cuidado del cabello', 'Cuidado de la piel', 'Make-up']
+
+export const Nav = () => {
+    return (
+        // eslint-disable
+        <nav className="navbar navbar-expand-lg navbar-light barraNavegacion">
+            <div className="container-fluid">
+                <img className="logo" src="/imagenes/logo-beauty2.png" alt="logo" />
+                <a class="navbar-brand nombreTienda" href="#">Tienda Beauty</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>                        
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">{menu[0]}</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">{menu[1]}</a>
+                        </li>                               
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">{menu[2]}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
